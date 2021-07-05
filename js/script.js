703aef9a38bf672d90b842978eab0497df06b1ca
ekaterina_kusa <kusa2706@gmail.com>
2021-07-05 10:53:01 +0300

    Делает кроссбраузерность, Пиксель перфект, попап и карту интерактивную
    Привет) У меня проблема с интерактивной картой и обычной. При отключении интерактивной карты - кнопка прыгает на картинку (так как картинка абсолют). Что то не могу сообразить что сделать.
    И с попапом карты косяк. На интерактивную карту почему то если нажать - попап не открывается. А если на картинку нажать - то открывается.
    
    И еще тупой вопросЮ На главной странице где 6 иконок с товарами, как сделать "фитнес-браслет" в одну строчку? уже все перепробовала....

--- js/script.js
+++ js/script.js
@@ -6,6 +6,10 @@ var form = popup.querySelector("[name=userfield-form]");
 var username = popup.querySelector("[name=username]");
 var email = popup.querySelector("[name=user-mail]");
 var message = popup.querySelector("[name=user-message]");
+var link_map = document.querySelector(".contacts__map");
+var popup_map = document.querySelector(".modal-map");
+var area_map = popup_map.querySelector(".modal-map__container");
+var close_map = popup_map.querySelector(".modal-map__button");
 
 
 link.addEventListener("click", function (evt) {
@@ -14,10 +18,20 @@ link.addEventListener("click", function (evt) {
   area.classList.add("modal-show-animation");
   username.focus();
 });
+link_map.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup_map.classList.add("modal-map-show");
+  area_map.classList.add("modal-map-show-animation");
+});
 close.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.remove("modal-show");
   popup.classList.remove("modal-error");
+});
+close_map.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup_map.classList.remove("modal-map-show");
+  popup_map.classList.remove("modal-map-error");
 });
 popup.addEventListener("click", function (evt) {
   if (!evt.target.classList.contains('modal')) {
@@ -26,6 +40,14 @@ popup.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.remove("modal-show");
   popup.classList.remove("modal-error");
+});
+popup_map.addEventListener("click", function (evt) {
+  if (!evt.target.classList.contains('modal-map')) {
+    return;
+  }
+  evt.preventDefault();
+  popup_map.classList.remove("modal-map-show");
+  popup_map.classList.remove("modal-map-error");
 });
 form.addEventListener("submit", function (evt) {
   if (!username.value || !email.value || !message.value) {
@@ -41,6 +63,15 @@ window.addEventListener("keydown", function (evt) {
     if (popup.classList.contains("modal-show")) {
       popup.classList.remove("modal-show");
       popup.classList.remove("modal-error");
+    }
+  }
+});
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (popup_map.classList.contains("modal-map-show")) {
+      popup_map.classList.remove("modal-map-show");
+      popup_map.classList.remove("modal-map-error");
     }
   }
 });
